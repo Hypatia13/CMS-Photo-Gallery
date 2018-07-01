357f16f2b67adbe9b8182fdeeaa22b8b2948efd1
Erie-Mac <ditrich13@gmail.com>
2018-07-01 10:13:26 +0300

    DB connection fix

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,4 +1,4 @@
-<?
+<?php
 //Database Connection Constants
 
 define('DB_HOST','localhost');
@@ -6,7 +6,15 @@ define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','gallery_db');
 
+// $servName = "localhost:3307";
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
-$connection = mysqli_connect('DB_HOST','DB_USER','DB_PASS','DB_NAME');
+//To check the connection
+if ($connection) {
+    echo "true";
+}
 
+    //If connectivity problems, cause the port was changed to 3307
+// $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME,'3307','/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock');
+// $dbconn = new mysqli('localhost:3307', /*your username*/, /*your password*/, /*your database name*/);
 ?>
