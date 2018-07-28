ff288f78d406880db726cc31ba23fd37d90aa878
Erie-Mac <ditrich13@gmail.com>
2018-07-28 18:56:43 +0300

    Instantiation method to find all users

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -10,7 +10,7 @@ class User {
 
     public static function find_all_users() {
       /*   global $database;
-        $result_set = $database->query("SELECT * FROM users"); //Replace with a universal query instead */
+        $result_set = $database->query("SELECT * FROM users"); //Replaced with a universal query instead */
 
         return self::find_this_query("SELECT * FROM users");
     }
@@ -18,7 +18,8 @@ class User {
     //Very unsecure way! Need to use prepared statements instead!!!
     public static function find_user_by_id($user_id) {
        /*  global $database;
-        $result_set = $database->query("SELECT * FROM users WHERE id = $user_id LIMIT 1"); */
+        $result_set = $database->query("SELECT * FROM users WHERE id = $user_id LIMIT 1"); 
+        //Replaced with a universal query instead */
 
         $result_set = self::find_this_query("SELECT * FROM users WHERE id = $user_id LIMIT 1");
         $found_user = mysqli_fetch_array($result_set); //Fetch a result row as an associative/numeric array
@@ -29,7 +30,7 @@ class User {
     public static function find_this_query($sql) {
         global $database;
         $result_set = $database->query($sql);
-
+        
         $the_object_array = array(); //An empty array with objects
 
         while ($row = mysqli_fetch_array($result_set)){
@@ -39,18 +40,19 @@ class User {
         return $the_object_array;
     }
 
-    //A bit unclear still
-    public static function instantiation($the_record) { //$the_record === $the_found_user
-        $the_object = new self; //?????
 
-            // Do instantiation with an array instead
+    // Should be private, but public for testing purposes
+    public static function instantiation($the_record) { //$the_record === $the_found_user
+        $the_object = new self; //Referring to itself
+
+            // Do instantiation with a loop instead
         /* $the_object -> id = $found_user['id'];
         $the_object -> username = $found_user['username'];
         $the_object -> password = $found_user['password'];
         $the_object -> first_name = $found_user['first_name'];
         $the_object -> last_name = $found_user['last_name']; */
 
-            //Loop through $the_record & use columns/values & assign them to keys/values in an array
+        //Loop through $the_record & use columns/values & assign them to keys/values in an array
         foreach ($the_record as $property => $value) { //as: $key=>value pairs from an array
             if($the_object->has_the_property($property)) { //if object has the same property, as an array's key 
                 $the_object->property = $value; //Assign a value from an array to an object's property
@@ -59,9 +61,9 @@ class User {
         return $the_object;
     }
 
-    private function has_the_property($property) {
+private function has_the_attribute($the_attribute) {
         $object_properties = get_object_vars($this); //Returns all object's properties in an array
-        return array_key_exists($property, $object_properties); //Check for a property inside of an array
+        return array_key_exists($the_attribute, $object_properties); //Check for a property inside of an array
     }
 
 }
